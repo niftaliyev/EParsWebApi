ff6d99082eea2e7daed610c466549580ca87036f
Kamran <Kamran02395@mail.ru>
2021-08-25 17:03:00 -0700

    firstInitial

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApi.Helpers;
+using WebApi.Jobs;
+using WebApi.Repository;
+using WebApi.Services;
+using WebApi.Services.TapAz;
 
 namespace WebApi
 {
@@ -23,26 +21,46 @@ namespace WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddTransient(x => new UnitOfWork("Server=localhost;Port=3306;Uid=weatherapi;Pwd=1n@pFmGImnXiQdTWw;Database=weatherapi;SslMode = none;"));
+            services.AddTransient(x => new UnitOfWork("Server=localhost;Port=3306;Uid=root;Pwd='';Database=emlaksoon;SslMode = none;"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EParseWebApi", Version = "v1" });
             });
+
+
+            services.AddHttpClient();
+
+            services.AddTransient<FileUploadHelper>();
+            services.AddTransient<TapAzParser>();
+            services.AddTransient<EmlakBaza>();
+            services.AddTransient<TapAzImageUploader>();
+            services.AddTransient<HttpClientCreater>();
+
+
+            services.AddTransient<RecordsJob>();
+            services.AddHostedService<RecordsScheduleService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
