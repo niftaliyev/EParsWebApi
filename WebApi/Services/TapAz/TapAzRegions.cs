4e5f7acca82cff886ea455e469d4154961f37180
Kamran <kamran02395@gmail.com>
2021-10-28 07:20:20 -0700

    stage files

---
+++ WebApi/Services/TapAz/TapAzRegions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Services.TapAz
+{
+    public class TapAzRegions
+    {
+        Dictionary<string, int> regionNames = new Dictionary<string, int>();
+
+
+        public TapAzRegions()
+        {
+            regionNames.Add("Abşeron r.",1);
+            regionNames.Add("Xətai ray.", 3);
+            regionNames.Add("Nərimanov r.",6);
+            regionNames.Add("Səbail r.",11);
+            regionNames.Add("Xəzər ray.", 4);
+            regionNames.Add("Nəsimi r.",7);
+            regionNames.Add("Suraxanı r.",12);
+            regionNames.Add("Binəqədi r.",2);
+            regionNames.Add("Nizami r.",8);
+            regionNames.Add("Pirallahı r.",9);
+            regionNames.Add("Qaradağ r.",5);
+            regionNames.Add("Sabunçu r.",10);
+            regionNames.Add("Yasamal r.",13);
+            regionNames.Add("Насиминский р.",7);
+        }
+
+        public Dictionary<string, int> GetRegionsNamesAll()
+        {
+            return regionNames;
+        }
+    }
+}
---
+++ WebApi/SingletonProxyServersIp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class SingletonProxyServersIp
+    {
+        static private string[] proxies = null;
+
+        static public string[] Instance
+        {
+            get
+            {
+                if (proxies == null)
+                    proxies = File.ReadAllLines("proxies.txt");
+                return proxies;
+            }
+        }
+
+        public List<int> Data { get; set; }
+
+        private SingletonProxyServersIp()
+        {
+            proxies = File.ReadAllLines("proxies.txt");
+
+        }
+    }
+}
